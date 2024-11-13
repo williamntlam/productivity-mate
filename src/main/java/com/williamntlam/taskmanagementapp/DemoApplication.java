9b6cc1f111cb412e0cb502718ab53562aabd1dfa
William Nhut Lam <williamntlam@gmail.com>
2024-11-12 19:55:01 -0500

    chore: correct .gitignore file

--- src/main/java/com/williamntlam/taskmanagementapp/DemoApplication.java
+++ src/main/java/com/williamntlam/taskmanagementapp/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.williamntlam.demo;
+package com.williamntlam.taskmanagementapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
