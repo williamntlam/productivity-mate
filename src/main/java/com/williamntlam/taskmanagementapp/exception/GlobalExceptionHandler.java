32be8b50cc91c08c7803bf428b2c1cf351241495
William Nhut Lam <williamntlam@gmail.com>
2024-11-19 23:10:12 -0500

    feat: implemented GlobalExceptionHandler.java

--- src/main/java/com/williamntlam/taskmanagementapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/williamntlam/taskmanagementapp/exception/GlobalExceptionHandler.java
@@ -1,8 +1,32 @@
 package com.williamntlam.taskmanagementapp.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+
+        return ResponseEntity.badRequest().body(exception.getMessage());
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> 
+            errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+
+    }
+
 }
