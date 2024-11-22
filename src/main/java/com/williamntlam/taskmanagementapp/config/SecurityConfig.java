0d776dd33b995bfe0f7594b8b09edcf2bced340f
William Nhut Lam <williamntlam@gmail.com>
2024-11-22 14:20:58 -0500

    feat: added protected routes with JWTs

--- src/main/java/com/williamntlam/taskmanagementapp/config/SecurityConfig.java
+++ src/main/java/com/williamntlam/taskmanagementapp/config/SecurityConfig.java
@@ -1,18 +1,41 @@
 package com.williamntlam.taskmanagementapp.config;
 
+import com.nimbusds.oauth2.sdk.auth.JWTAuthentication;
+import com.williamntlam.taskmanagementapp.util.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import com.williamntlam.taskmanagementapp.filter.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()) // Disable CSRF
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Allow all requests
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Allow all requests
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
         return http.build();
     }
+
+    
+
 }
 
---
+++ src/main/java/com/williamntlam/taskmanagementapp/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.williamntlam.taskmanagementapp.filter;
+
+import com.williamntlam.taskmanagementapp.util.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
+    private final JwtUtils jwtUtils;
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils) {
+
+        this.jwtUtils = jwtUtils;
+
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // Extract Authorization Header
+        String authHeader = request.getHeader("authorization");
+
+        // Check if it contains a Bearer token
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            String token = authHeader.substring(7); // Extract the token
+
+            // Validate the token
+            if (jwtUtils.validateToken(token)) {
+
+                // Extract the username from the token
+                String username = jwtUtils.extractUsername(token);
+
+                // Create an authentification object and set it in the SecurityContext
+                UsernamePasswordAuthenticationToken authentification = new UsernamePasswordAuthenticationToken(username, null, null);
+                authentification.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentification);
+
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
