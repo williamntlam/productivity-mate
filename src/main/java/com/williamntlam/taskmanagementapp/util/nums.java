7810ec80429483e888ed79047e9e1c8388d99302
William Nhut Lam <williamntlam@gmail.com>
2024-12-04 21:36:40 -0500

    feat: added enums.java

---
+++ src/main/java/com/williamntlam/taskmanagementapp/util/nums.java
@@ -0,0 +1,15 @@
+package com.williamntlam.taskmanagementapp.util;
+
+public class nums {
+    
+    // Task priorities
+    public enum TaskPriority {
+        LOW, MEDIUM, HIGH
+    }
+
+    // Task statuses
+    public enum TaskStatus {
+        PENDING, IN_PROGRESS, COMPLETED
+    }
+
+}
