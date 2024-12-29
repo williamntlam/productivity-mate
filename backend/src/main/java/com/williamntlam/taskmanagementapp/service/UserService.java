2c04dd87e40ff0af0b6da3be6c7531f0b1b8102d
William Nhut Lam <williamntlam@gmail.com>
2024-12-29 15:55:04 -0500

    remove: removed password logic in UserService and UserController

--- backend/src/main/java/com/williamntlam/taskmanagementapp/service/UserService.java
+++ backend/src/main/java/com/williamntlam/taskmanagementapp/service/UserService.java
@@ -2,19 +2,16 @@ package com.williamntlam.taskmanagementapp.service;
 
 import com.williamntlam.taskmanagementapp.model.User;
 import com.williamntlam.taskmanagementapp.repository.UserRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
   private final UserRepository userRepository;
-  private final BCryptPasswordEncoder passwordEncoder;
 
   public UserService(UserRepository userRepository) {
 
     this.userRepository = userRepository;
-    this.passwordEncoder = new BCryptPasswordEncoder();
   }
 
   public User findById(Long userId) {
@@ -25,23 +22,7 @@ public class UserService {
 
   // Create User for registration
   public User registerUser(User user) {
-
-    // Hash the password before saving the user.
-    String hashedPassword = passwordEncoder.encode(user.getPassword());
-    user.setPassword(hashedPassword);
-
     return userRepository.save(user);
-  }
-
-  // Validate User Credentials
-  public boolean loginUser(String username, String rawPassword) {
-
-    User user =
-        userRepository
-            .findByEmail(username)
-            .orElseThrow(() -> new IllegalArgumentException("User not found"));
-
-    return passwordEncoder.matches(rawPassword, user.getPassword());
   }
 
   public boolean emailExists(String email) {
